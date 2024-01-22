19a7416417fe888490f3fb9c51667888fb8abdd3
Bryce Langford <bryceslangford@gmail.com>
2024-01-22 15:09:52 -0700

    Wrote code for resume prepare 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1.company = "Tel Daco";
+        job1.jobTitle = "Cheese Melter";
+        job1.startYear = 2045;
+        job1.endYear = 2099;
+
+        Job job2 = new Job();
+        job2.company = "Google";
+        job2.jobTitle = "Software Tech Director";
+        job2.startYear = 2099;
+        job2.endYear = 3010;
+        
+        
+        Resume resume1 = new Resume();
+        resume1._name = "Merlin Ambrosius";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
     }
 }
