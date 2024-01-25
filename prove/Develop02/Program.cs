61efcb3837bb4bdddc06d822fb0b1543fe912b76
Bryce Langford <bryceslangford@gmail.com>
2024-01-24 19:11:14 -0700

    feat: Added to the class system

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,4 +6,39 @@ class Program
     {
         Journal journal = new Journal();
 
-        while (tru
+        while (true)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("Enter your choice (1-5): ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    journal.AddEntry();
+                    break;
+                case "2":
+                    journal.DisplayEntries();
+                    break;
+                case "3":
+                    journal.SaveToFile();
+                    break;
+                case "4":
+                    journal.LoadFile();
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.\n");
+                    break;
+            }
+        }
+    }
+}
--- prove/Develop02/PromptGen.cs
+++ prove/Develop02/PromptGen.cs
@@ -1,3 +1,13 @@
 public class PromptGen {
 
-    
+    public List<string> prompts = new List<string>
+        {
+            "Who was the most interesting person I interacted with today?",
+            "What was the best part of my day?",
+            "How did I see the hand of the Lord in my life today?",
+            "What was the strongest emotion I felt today?",
+            "If I had one thing I could do over today, what would it be?",
+        };
+
+
+}
